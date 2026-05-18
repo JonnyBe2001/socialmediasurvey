eac0119806740d60ce4b6c6206040882b4c5da3a
Jonas Bauer <jonas.bauer90@outlook.de>
2026-05-18 14:22:12 +0200

    added: comments

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ var group;
 random();
 
 function random () {
-    group = Math.random();
+    group = Math.random(); // Generates a random number between 0 and 1
     redirect();
 }
 
 function redirect () {
     if (group < 0.5) {
-        window.location.href = "https://github.com";
+        window.location.href = "https://github.com"; // Change this to your desired URL for group A
     } else {
-        window.location.href = "https://google.com";
+        window.location.href = "https://google.com"; // Change this to your desired URL for group B
     }
 }
